9fbdf7f6f2944c7331a2adb036b56e157881981d
iambillgates <150753840+iambillgates@users.noreply.github.com>
2026-05-11 23:31:35 +0700

    update

--- ConstraintLayout/app/src/main/java/com/dwiakbar/constraintlayout/MainActivity.java
+++ ConstraintLayout/app/src/main/java/com/dwiakbar/constraintlayout/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dwiakbar.constraintlayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnToActivity2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnToActivity2 = findViewById(R.id.btnToActivity2);
+
+        btnToActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
